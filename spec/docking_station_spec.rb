bef2bb4cb18a033be44ce67ece27fe4c3ad57302
kealanheena <kealanheena@gmail.com>
2020-03-31 00:43:24 +0100

    Chore: Add mocks and studs to tests

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -6,15 +6,16 @@ describe DockingStation do
       expect(subject).to respond_to(:release_bike)
     end
 
-    it "returns an instance of bike" do
-      bike = Bike.new
+    it "returns the bike" do
+      bike = double(:bike)
+      allow(bike).to receive(:broken?).and_return(false)
       subject.dock(bike)
-      expect(subject.release_bike).to be_instance_of Bike
+      expect(subject.release_bike).to eq bike
     end
     
     it "doesn't realease a broken bike" do
-      bike = Bike.new
-      bike.report
+      bike = double(:bike)
+      allow(bike).to receive(:broken?).and_return(true)
       subject.dock(bike)
       expect { subject.release_bike }.to raise_error("This bike is broken")
     end
@@ -22,24 +23,31 @@ describe DockingStation do
     it "gives an error message if there is no biked docked" do
       expect { subject.release_bike }.to raise_error("There is no bike available!")
     end
+
+    it 'releases working bikes' do
+      bike = double(:bike)
+      subject.dock(bike)
+      allow(bike).to receive(:broken?).and_return(false)
+      expect(subject.release_bike).not_to be_broken
+    end
   end
 
   describe "#dock" do
     it "stores Bike instances in an instance of DockingStation" do 
-      bike = Bike.new
+      bike = double(:bike)
       expect(subject.dock(bike)).to eq [bike]
     end
 
     it "should be able to dock the default capacity of 20 bikes" do
       station = DockingStation.new
-      (DockingStation::DEFAULT_CAPACITY).times{ station.dock(Bike.new)}
+      (DockingStation::DEFAULT_CAPACITY).times{ station.dock(double(:bike))}
       expect(station.bikes.count).to eq(20)
     end
   end
 
   describe "#dock" do
     it "check if the DockingStation has a bike" do 
-      bike = Bike.new
+      bike = double(:bike)
       subject.dock(bike)
       expect(subject.bikes).to eq [bike]
     end
@@ -49,22 +57,22 @@ describe DockingStation do
     end
 
     it "gives an error if capacity is full" do
-      expect { (DockingStation.new::capacity + 1 ).times { subject.dock(Bike.new) } }.to raise_error("There is no space available!")
+      expect { (DockingStation.new::capacity + 1 ).times { subject.dock(double(:bike)) } }.to raise_error("There is no space available!")
     end
 
     it "gives an error if capacity is full" do
       station = DockingStation.new(19)
-      expect { (station::capacity + 1 ).times { station.dock(Bike.new) } }.to raise_error("There is no space available!")
+      expect { (station::capacity + 1 ).times { station.dock(double(:bike)) } }.to raise_error("There is no space available!")
     end
 
     it "gives an error if capacity is full" do
       station = DockingStation.new(21)
-      expect { (station::capacity + 1 ).times { station.dock(Bike.new) } }.to raise_error("There is no space available!")
+      expect { (station::capacity + 1 ).times { station.dock(double(:bike)) } }.to raise_error("There is no space available!")
     end
 
     it 'should allow broken bikes to be docked' do
-      bike = Bike.new
-      bike.report
+      bike = double(:bike)
+      allow(bike).to receive(:broken?).and_return(true)
       expect(subject.dock(bike)).to eq [bike]
     end
   end
