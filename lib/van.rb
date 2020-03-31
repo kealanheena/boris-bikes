5742158ff0c532309cc14cf91c78acbada4ab14d
kealanheena <kealanheena@gmail.com>
2020-03-31 03:08:38 +0100

    Chore: Make van collect only broken bikes

--- lib/van.rb
+++ lib/van.rb
@@ -1,6 +1,13 @@
 class Van
+  def initialize
+    @broken_bikes = []
+  end
+
   def collect(container)
-    container.bikes
+    container.bikes.each { |bike| 
+      @broken_bikes << bike if bike.broken? == true
+    }
+    @broken_bikes
   end
 
 end
--- spec/van_spec.rb
+++ spec/van_spec.rb
@@ -4,13 +4,20 @@ describe Van do
 
   before(:each) do
     @bike = double(:bike)
+    @bike2 = double(:bike)
     allow(@bike).to receive(:broken?) { true }
+    allow(@bike2).to receive(:broken?) { false }
     @dockingstation = double(:dockingstation)
-    allow(@dockingstation).to receive(:bikes) { [@bike] }
+    allow(@dockingstation).to receive(:bikes) { [@bike, @bike2] }
   end
 
   describe '#collect' do
     it 'should collect broken bikes from the docking station' do
+      van = Van.new
+      expect(van.collect(@dockingstation)).to eq [@bike]
+    end
+
+    it 'should only collect the broken bikes' do 
       van = Van.new
       expect(van.collect(@dockingstation)).to eq [@bike]
     end
